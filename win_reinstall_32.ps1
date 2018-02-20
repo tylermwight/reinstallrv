e8918058c904541b813eeb76bad39ec9167bd9ec
tylermwight <tyler.wight@risevision.com>
2018-02-20 13:18:48 -0700

    fixed windows issue

--- win_reinstall_32.ps1
+++ win_reinstall_32.ps1
@@ -4,21 +4,21 @@ $date = (Get-Date).ToString("yyyy-MM-dd")
 $test = Test-Path "$HOME\AppData\local\rvplayer_$date"
 $test1 = Test-Path "$HOME\AppData\local\rvplayer"
 
+Write-Output "date is: $date"
 Write-Output "rvplayer_date check: $test"
 Write-Output "rvplayer check: $test1"
 
+if (Test-Path "$HOME\AppData\local\rvplayer_$date"){
+	Remove-Item -Path "$HOME\AppData\local\rvplayer_$date" -recurse
+}
+
 if (Test-Path "$HOME\Appdata\local\rvplayer"){
-	Rename-Item -Path "$HOME\Appdata\local\rvplayer" -NewName "rvplayer_$date" -ErrorAction Stop
+	Rename-Item -Path "$HOME\Appdata\local\rvplayer" -NewName "rvplayer_$date"
 }else{
 	Write-Output "rvplayer not installed"
 	exit
 }
 	
-if (Test-Path "$HOME\AppData\local\rvplayer_$date"){
-	Remove-Item -Path "$HOME\AppData\local\rvplayer_$date" -recurse
-}
-	
-
 Invoke-WebRequest -Uri $url -OutFile $outpath
 Start-Process -Filepath $outpath
 
