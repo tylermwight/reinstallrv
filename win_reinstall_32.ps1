7cceba87adfa69e6fb84c238542b81c5c5a65105
tylermwight <tyler.wight@risevision.com>
2018-02-20 16:26:46 -0700

    Keep Display ID and delete Rise Cache on Windows

--- win_reinstall_32.ps1
+++ win_reinstall_32.ps1
@@ -1,12 +1,6 @@
 $url = "http://install-versions.risevision.com/installer-win-32.exe"
 $outpath = "$PSScriptRoot/installer-win-32.exe"
 $date = (Get-Date).ToString("yyyy-MM-dd")
-$test = Test-Path "$HOME\AppData\local\rvplayer_$date"
-$test1 = Test-Path "$HOME\AppData\local\rvplayer"
-
-Write-Output "date is: $date"
-Write-Output "rvplayer_date check: $test"
-Write-Output "rvplayer check: $test1"
 
 if (Test-Path "$HOME\AppData\local\rvplayer_$date"){
 	Remove-Item -Path "$HOME\AppData\local\rvplayer_$date" -recurse
@@ -14,16 +8,14 @@ if (Test-Path "$HOME\AppData\local\rvplayer_$date"){
 
 if (Test-Path "$HOME\Appdata\local\rvplayer"){
 	Rename-Item -Path "$HOME\Appdata\local\rvplayer" -NewName "rvplayer_$date"
+	Remove-Item -Path "$HOME\Appdata\local\rvplayer_$date\RiseCache" -recurse
 }else{
 	Write-Output "rvplayer not installed"
 	exit
 }
-	
+
+New-Item -ItemType Directory -Path "$HOME\appdata\local\rvplayer" -Force
+Copy-Item "$HOME\appdata\local\rvplayer_$date\RiseDisplayNetworkII.ini" -Destination "$HOME\appdata\local\rvplayer\"
+
 Invoke-WebRequest -Uri $url -OutFile $outpath
 Start-Process -Filepath $outpath
-
-if ($Host.Name -eq "ConsoleHost")
-{
-    Write-Host "Press any key to continue..."
-    $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyUp") > $null
-}
